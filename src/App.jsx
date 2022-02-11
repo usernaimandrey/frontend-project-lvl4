eb2147440fbc971212ccc5ae6e348f7f2bc11328
usernaimandrey <anshlyapnikov@yandex.ru>
2022-02-11 13:17:52 +0300

    test rollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import { Provider as ProviderRollbar, ErrorBoundary } from '@rollbar/react';
+import { Provider as ProviderRollbar, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
 import store from './slices/index.js';
 import './i18n.js';
 import authContext from './context/index.jsx';
@@ -25,10 +25,14 @@ const App = () => {
   const rollbarConfig = {
     accessToken: '2ed00c04002f46748e0ef17039c4b0a1',
     environment: 'production',
+    enabled: process.env.NODE_ENV === 'production',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
   };
+  setTimeout(function() {notThere();}, 1000);
   return (
     <ProviderRollbar config={rollbarConfig}>
-      <ErrorBoundary>
+      <ErrorBoundary level={LEVEL_WARN}>
         <AuthProvider>
           <NavBar />
         </AuthProvider>
